ca738484ff7b6f64ff844a5aa44f6b60f7e07964
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-04-22 08:43:16 +0530

    [Rahul]Program using object and class concepts

--- OopsConceptProblems/Program.cs
+++ OopsConceptProblems/Program.cs
@@ -7,6 +7,10 @@ namespace OopsConceptProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Oops Concept Problems");
+            ClassName name = new ClassName();
+            name.Name();
+            //halt program execution until the user presses a key 
+            //Console.ReadKey();
         }
     }
 }
